bdcb9534ae5c588ae26895e0a8ee4403a62671b0
Hugh Bowie IV <hughbowie@me.com>
2021-06-04 13:53:53 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const express = require("express");
+require("dotenv").config();
