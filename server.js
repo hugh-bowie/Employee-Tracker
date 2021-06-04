50f0cceaefe4dd8d9cbbf1e339747b7e1e6c57f8
Hugh Bowie IV <hughbowie@me.com>
2021-06-04 13:54:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const express = require("express");
 require("dotenv").config();
+
+const app = express();
+const PORT = 3001;
