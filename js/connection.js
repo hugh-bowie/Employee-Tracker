a9c3f417ca75ece8e735cac130dc7ef985257174
Hugh Bowie IV <hughbowie@me.com>
2021-06-07 23:09:42 -0400

    ✏️

--- js/connection.js
+++ js/connection.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 const connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
-	user: 'root',
+	user: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: 'employees',
 });
